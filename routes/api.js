ab76c8278f68049e09df088a7df6db923171ee6a
lx <berndtae@gmail.com>
2019-02-06 00:20:16 +0100

    Working frontend using expressjs framework

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET api listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+  res.send('API endpoint');
 });
 
 module.exports = router;
